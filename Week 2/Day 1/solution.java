661492068397537a2941eae7b83c8e6c6924a397
Sunil Kumar <sunil32.kumar@paytm.com>
2020-06-08 22:37:11 +0530

    Power of Two

---
+++ Week 2/Day 1/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        
+        while(n>1 && n%2 == 0){
+            n = n>>1;
+        }
+        return (n == 1) ? true : false;
+    }
+}
